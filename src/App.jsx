717f74cde9a4e79e022a220fc81346d92030b925
=raj-maharjan99 <=rjuallone@gmail.com>
2024-05-21 02:10:11 -0500

    install react router dom to provide router for component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Home from "./pages/Home";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import RootLayout from "./shared_component/RootLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+
+      { path: "*", element: <Error /> },
+    ],
+  },
+]);
 
 const App = () => {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 };
-
 export default App;

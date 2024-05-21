6a42dcc40e8147eb78d90df9a7667d68dc86acf7
=raj-maharjan99 <=rjuallone@gmail.com>
2024-05-21 02:54:26 -0500

    setup routes for navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import Home from "./pages/Home";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import RootLayout from "./shared_component/RootLayout";
+import ErrorPage from "./shared_component/ErrorPage";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Gallery from "./pages/Gallery";
 
 const router = createBrowserRouter([
   {
@@ -12,8 +16,20 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home />,
       },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/gallery",
+        element: <Gallery />,
+      },
 
-      { path: "*", element: <Error /> },
+      { path: "*", element: <ErrorPage /> },
     ],
   },
 ]);
--- src/shared_component/Header.jsx
+++ src/shared_component/Header.jsx
@@ -1,7 +1,58 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header page</div>;
+  return (
+    <div className="flex justify-between items-center px-5 py-2">
+      <div className="flex gap-4 justify-center items-center">
+        {" "}
+        <div className="w-8 h-8 bg-sky-500 rounded-full"></div>
+        <div className="font-bold font-sans">SAROJ KAPALI</div>
+        <div className="text-[10px] text-neutral-500">
+          Data Science & React Developer
+        </div>
+      </div>
+      <div>
+        <ul className="flex gap-2 text-gray-500">
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? "nav_link text-red-600 underline" : ""
+            }
+            to={"/"}
+          >
+            <li>Home</li>
+          </NavLink>
+          <li>|</li>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? "text-red-600 underline" : ""
+            }
+            to={"/about"}
+          >
+            <li>About Me</li>
+          </NavLink>
+          <li>|</li>
+          <NavLink
+            to={"/contact"}
+            className={({ isActive }) =>
+              isActive ? "text-red-600 underline" : ""
+            }
+          >
+            <li>Contact</li>
+          </NavLink>
+          <li>|</li>
+          <NavLink
+            to={"/gallery"}
+            className={({ isActive }) =>
+              isActive ? "text-red-600 underline" : ""
+            }
+          >
+            <li>Gallery</li>
+          </NavLink>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Header;

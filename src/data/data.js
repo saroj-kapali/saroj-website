4b2e671f2cf2ac9f6c2780571038105319b2d7c3
SarojKapali <81839032+saroj-kapali@users.noreply.github.com>
2024-05-21 19:28:27 -0500

    footer updated

---
+++ src/data/data.js
@@ -0,0 +1,6 @@
+export const footerData = [
+  { id: 1, title: "Email", content: "sarojkapali.official@gmail.com" },
+  { id: 2, title: "Linkedin", content: "https://www.linkedin.com/in/sa-roj/" },
+  { id: 3, title: "GitHub", content: "https://github.com/saroj-kapali" },
+  
+];
--- src/shared_component/Footer.jsx
+++ src/shared_component/Footer.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { footerData } from "../data/data";
 
 const Footer = () => {
-  return <div>Footer Page</div>;
+  const date = new Date();
+  const year = date.getFullYear();
+
+  return <div className="flex justify-between items-center px-2">
+    <div className="flex gap-20 justify-between w-[75%]">
+    {footerData.map((a)=>(
+      <div key={a.id} className="flex flex-col">
+      <h2 className="font-bold">{a.title}</h2>
+      <p className="text-[10px] text-gray-500">{a.content}</p>
+    </div>
+    ))} 
+    </div>
+    <div className="text-[10px] flex flex-col text-gray-500 text-right w-[25%]">
+      <p>&copy; {year} By Saroj Kapali</p>
+      <p>Created By Raj Maharjan</p>
+    </div>
+  </div>;
 };
 
 export default Footer;
